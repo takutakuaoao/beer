6bf27383e9a3cf24a0cf57a5be6c69db2b8715c6
aochan <nakinagaraodoru1013@gmail.com>
2024-10-30 06:41:45 +0000

    🚀 Add for showing struct properties as debug.

--- app/printer.go
+++ app/printer.go
@@ -10,7 +10,106 @@ type Printer struct {
 }
 
 func (p *Printer) Write() string {
-	o := reflect.TypeOf(p.item)
+	object := NewStruct(reflect.TypeOf(p.item), reflect.ValueOf(p.item))
+	content := *NewContent("")
 
-	return fmt.Sprintf("(%v) %v {}", o.Kind(), o.Name())
+	if object.HasProperty() {
+		content = object.WriteProperties(content)
+	}
+
+	return object.WriteStruct(content).content
+}
+
+func NewStruct(reflectType reflect.Type, reflectValue reflect.Value) *Struct {
+	return &Struct{
+		reflectType:  reflectType,
+		reflectValue: reflectValue,
+	}
+}
+
+type Struct struct {
+	reflectType  reflect.Type
+	reflectValue reflect.Value
+}
+
+func (s *Struct) HasProperty() bool {
+	return s.reflectType.NumField() > 0
+}
+
+func (s *Struct) WriteStruct(content Content) Content {
+	identified := fmt.Sprintf("(%v) %v", s.getType(), s.getStructName())
+
+	return content.Surround("{", "}").AddHead(fmt.Sprintf("%v ", identified))
+}
+
+func (s *Struct) getStructName() string {
+	return s.reflectType.Name()
+}
+
+func (s *Struct) getType() string {
+	return s.reflectType.Kind().String()
+}
+
+func (s *Struct) WriteProperties(content Content) Content {
+	s.loopProperties(func(name string, value reflect.Value, propertyType string) {
+		content = content.Indent().Merge(
+			s.propertyText(name, value, propertyType),
+		)
+	})
+
+	return content.LineBreak()
+}
+
+func (s *Struct) loopProperties(callBack func(name string, value reflect.Value, propertyType string)) {
+	for i := 0; i < s.reflectType.NumField(); i++ {
+		field := s.reflectType.Field(i)
+
+		callBack(field.Name, s.reflectValue.FieldByName(field.Name), field.Type.Name())
+	}
+}
+
+func (s *Struct) propertyText(name string, value reflect.Value, propertyType string) string {
+	valueText := ""
+
+	if propertyType == "string" {
+		valueText = fmt.Sprintf("\"%v\"", value)
+	} else {
+		valueText = fmt.Sprintf("%v", value)
+	}
+
+	return fmt.Sprintf("%v (%v) %v", name, propertyType, valueText)
+}
+
+func NewContent(content string) *Content {
+	return &Content{
+		content: content,
+	}
+}
+
+type Content struct {
+	content string
+}
+
+func (c Content) Merge(merged string) Content {
+	result := fmt.Sprintf("%v%v", c.content, merged)
+
+	return *NewContent(result)
+}
+
+func (c Content) Indent() Content {
+	result := fmt.Sprintf("%v\n\t", c.content)
+
+	return *NewContent(result)
+}
+
+func (c Content) LineBreak() Content {
+	return c.Merge("\n")
+}
+
+func (c Content) Surround(pre string, last string) Content {
+	return *NewContent(fmt.Sprintf("%v%v%v", pre, c.content, last))
+}
+
+func (c Content) AddHead(text string) Content {
+	return *NewContent(fmt.Sprintf("%v%v", text, c.content))
 }
--- app/printer_test.go
+++ app/printer_test.go
@@ -16,5 +16,54 @@ func Test_to_show_empty_struct(t *testing.T) {
 	assert.Equal(t, "(struct) Empty {}", result)
 }
 
+func Test_to_show_properties_of_struct(t *testing.T) {
+	cases := []struct {
+		name   string
+		item   interface{}
+		expect string
+	}{
+		{
+			name: "with value",
+			item: HasProperty{
+				PublicProperty: "test",
+			},
+			expect: "(struct) HasProperty {\n\tPublicProperty (string) \"test\"\n}",
+		},
+		{
+			name:   "with non value",
+			item:   HasProperty{},
+			expect: "(struct) HasProperty {\n\tPublicProperty (string) \"\"\n}",
+		},
+		{
+			name: "with multiple properties",
+			item: HasMultipleProperties{
+				PublicProperty2: 4,
+			},
+			expect: "(struct) HasMultipleProperties {\n\tPublicProperty1 (string) \"\"\n\tPublicProperty2 (uint8) 4\n}",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			sut := Printer{
+				item: tt.item,
+			}
+
+			result := sut.Write()
+
+			assert.Equal(t, tt.expect, result)
+		})
+	}
+}
+
 type Empty struct {
+}
+
+type HasProperty struct {
+	PublicProperty string
+}
+
+type HasMultipleProperties struct {
+	PublicProperty1 string
+	PublicProperty2 uint8
 }
